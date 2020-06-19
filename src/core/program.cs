af702eeb6989b8dc9c1c4ae1cbe17c7fda881301
allan_walpy <allanwalpy@gmail.com>
2020-06-19 13:02:26 +0000

    change filename capitalizations

---
+++ src/core/program.cs
@@ -0,0 +1,12 @@
+﻿using System;
+
+namespace Macoreil.Core
+{
+    public class Program
+    {
+        public static void Main(string[] args)
+        {
+            Console.WriteLine("TODO:TMP: nothing here;");
+        }
+    }
+}
