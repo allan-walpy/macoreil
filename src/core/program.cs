7874f9bf68e3b1b9b0b45ca9ef16115bdcdb7833
allan_walpy <allanwalpy@gmail.com>
2020-06-20 22:00:33 +0000

    make `core` console project to worker one

--- src/core/program.cs
+++ src/core/program.cs
@@ -1,12 +1,26 @@
-using System;
+using Microsoft.Extensions.DependencyInjection;
+using Microsoft.Extensions.Hosting;
+using Microsoft.Extensions.Logging;
 
 namespace Macoreil.Core
 {
     public static class Program
     {
-        public static void Main(string[] args)
-        {
-            Console.WriteLine("TODO:TMP: nothing here;");
-        }
+        public static void Main(string[] args) =>
+            Program.CreateHostBuilder(args).Build().Run();
+
+        public static IHostBuilder CreateHostBuilder(string[] args) =>
+            Host.CreateDefaultBuilder(args)
+                .ConfigureServices((context, services) =>
+                {
+                    services.AddHostedService<Worker>();
+                })
+                .ConfigureLogging(loggingBuilder =>
+                {
+                    // TODO: check it works as `loggingBuilder.Clear();` in previous versions of dotnet;
+                    loggingBuilder.Services.Clear();
+                    loggingBuilder.AddConsole();
+                });
+
     }
 }
