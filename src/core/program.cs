5821f1ec09b5467c8710f353390568445e86c98c
allan_walpy <allanwalpy@gmail.com>
2020-06-20 22:07:57 +0000

    fix crash on no web host configured

--- src/core/program.cs
+++ src/core/program.cs
@@ -1,25 +1,20 @@
 using Microsoft.Extensions.DependencyInjection;
 using Microsoft.Extensions.Hosting;
-using Microsoft.Extensions.Logging;
 
 namespace Macoreil.Core
 {
     public static class Program
     {
-        public static void Main(string[] args) =>
-            Program.CreateHostBuilder(args).Build().Run();
+        public static void Main(string[] args)
+        {
+            CreateHostBuilder(args).Build().Run();
+        }
 
         public static IHostBuilder CreateHostBuilder(string[] args) =>
             Host.CreateDefaultBuilder(args)
                 .ConfigureServices((context, services) =>
                 {
                     services.AddHostedService<Worker>();
-                })
-                .ConfigureLogging(loggingBuilder =>
-                {
-                    // TODO: check it works as `loggingBuilder.Clear();` in previous versions of dotnet;
-                    loggingBuilder.Services.Clear();
-                    loggingBuilder.AddConsole();
                 });
 
     }
